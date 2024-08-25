adb4a3d7f51226a72e5aaab4fe54c119d867fbda
franclobo <fjbl2788@gmail.com>
2024-08-25 18:14:00 -0500

    Edit paypal initializers

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,6 +1,6 @@
 PayPal::Recurring.configure do |config|
-  config.sandbox = false
-  config.username = "fjbl2788_api1.gmail.com"
-  config.password = "FTFGN3AH5EDP4A22"
-  config.signature = "A21kmBLyRcQpDSjYvmswlm3ckjahA3AL59tcYlualHF9y0Xr.CXDJ6cu"
+  config.sandbox = true
+  config.username = ENV['PAYPAL_SANDBOX_USERNAME'].to_s
+  config.password = ENV['PAYPAL_SANDBOX_PASSWORD'].to_s
+  config.signature = ENV['PAYPAL_SANDBOX_SIGNATURE'].to_s
 end
